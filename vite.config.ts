6189707432ab9a04efb55f59e4f991230313ef37
Developer <developer@example.com>
2026-02-24 19:40:08 -0500

    Use docs folder for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/CSCIA614-Assignment-4/',
-  ssr: false,
+  build: {
+    outDir: 'docs'
+  }
 })
